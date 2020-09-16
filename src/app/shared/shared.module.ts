fb139f37466ab638348f76766c7b9d006b5b0b72
HamzaKhalidMirza <hamzakhalid.zx@gmail.com>
2020-09-16 12:39:42 +0500

    Created Basic SharedModule Setup

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { IonicModule } from "@ionic/angular";
+
+@NgModule({
+  imports: [CommonModule, IonicModule],
+  declarations: [],
+  exports: [CommonModule, IonicModule],
+})
+export class SharedModule {}
