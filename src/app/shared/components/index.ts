ef2feeb783db486aa077864ac8fe6bf8eccab6ba
HamzaKhalidMirza <hamzakhalid.zx@gmail.com>
2020-09-16 12:48:00 +0500

    Create Only SharedModule-Components (Favourite & ListView)

---
+++ src/app/shared/components/index.ts
@@ -0,0 +1,2 @@
+export { FavouriteComponent } from "./favourite/favourite.component";
+export { ListViewComponent } from "./list-view/list-view.component";
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { IonicModule } from "@ionic/angular";
+import { ListViewComponent, FavouriteComponent } from "./components/index";
 
 @NgModule({
   imports: [CommonModule, IonicModule],
-  declarations: [],
-  exports: [CommonModule, IonicModule],
+  declarations: [ListViewComponent, FavouriteComponent],
+  exports: [CommonModule, IonicModule, ListViewComponent, FavouriteComponent],
 })
 export class SharedModule {}
