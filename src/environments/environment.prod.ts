130f7eebb2b0b054f2f63514e34533c2fba10915
HamzaKhalidMirza <hamzakhalid.zx@gmail.com>
2020-09-16 12:51:46 +0500

    Set Firebase Configurations in Environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDmjuFphkFONgm5hZ1po24tcpbnMQpQ0bA",
+    authDomain: "asasa-app-login.firebaseapp.com",
+    databaseURL: "https://asasa-app-login.firebaseio.com",
+    projectId: "asasa-app-login",
+    storageBucket: "asasa-app-login.appspot.com",
+    messagingSenderId: "496250687458"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDmjuFphkFONgm5hZ1po24tcpbnMQpQ0bA",
+    authDomain: "asasa-app-login.firebaseapp.com",
+    databaseURL: "https://asasa-app-login.firebaseio.com",
+    projectId: "asasa-app-login",
+    storageBucket: "asasa-app-login.appspot.com",
+    messagingSenderId: "496250687458"
+  }
 };
 
 /*
